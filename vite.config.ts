39feec8714d6a14d6b4adb39c33fba8b246b7929
gusals072 <gusals072@ksmartpia.com>
2026-02-04 09:38:21 +0900

    fix: AXIOS 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,15 @@ export default defineConfig({
   plugins: [react()],
   base: '/ksp-itm-web/',
   server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
     // 모든 요청을 index.html로 리다이렉트하여 React Router가 처리하도록 함
     fallback: true,
   },
+
 })
